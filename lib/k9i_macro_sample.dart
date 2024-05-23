d242170020508572ae7e9f32734d531f733a61c3
K9i-0 <geukfyger@gmail.com>
2024-05-23 23:38:47 +0900

    refactor: declareInTypeを使った実装に変更

--- lib/k9i_macro_sample.dart
+++ lib/k9i_macro_sample.dart
@@ -7,36 +7,22 @@ import 'dart:async';
 import 'package:macros/macros.dart';
 
 macro class Hello implements ClassDeclarationsMacro {
-  const  Hello();
+  const Hello();
 
   @override
   FutureOr<void> buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder) async {
-    builder.declareInLibrary(
-      DeclarationCode.fromParts([
-        'augment ',
-        'class ${clazz.identifier.name}',
-        ' {',
-      ]),
-    );
-
     final methods = await builder.methodsOf(clazz);
     final hello =
         methods.where((e) => e.identifier.name == 'hello').firstOrNull;
 
     if (hello != null) return;
 
-    builder.declareInLibrary(
+    builder.declareInType(
       DeclarationCode.fromParts(
         [
           '  void hello() => print("Hello, World!");',
         ]
       ),
-    );
-
-    builder.declareInLibrary(
-      DeclarationCode.fromParts([
-        '}',
-      ]),
     );
   }
 }
