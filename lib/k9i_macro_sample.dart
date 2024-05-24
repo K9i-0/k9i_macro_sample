ae7af55575c070367f8100d7e3a490de2be89e10
K9i-0 <geukfyger@gmail.com>
2024-05-24 17:10:41 +0900

    fix: 3.5.0-163.0.dev以上でexportしたファイルのGo to Augmentationがでないバグが直った

--- lib/k9i_macro_sample.dart
+++ lib/k9i_macro_sample.dart
@@ -1,28 +1,3 @@
 library;
 
-// exportするとVS CodeにGo to Augmentationが表示されない？
-// export 'src/k9i_macro_sample_base.dart';
-import 'dart:async';
-
-import 'package:macros/macros.dart';
-
-macro class Hello implements ClassDeclarationsMacro {
-  const Hello();
-
-  @override
-  FutureOr<void> buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder) async {
-    final methods = await builder.methodsOf(clazz);
-    final hello =
-        methods.where((e) => e.identifier.name == 'hello').firstOrNull;
-
-    if (hello != null) return;
-
-    builder.declareInType(
-      DeclarationCode.fromParts(
-        [
-          '  void hello() => print("Hello, World!");',
-        ]
-      ),
-    );
-  }
-}
+export 'src/k9i_macro_sample_base.dart';
--- lib/src/k9i_macro_sample_base.dart
+++ lib/src/k9i_macro_sample_base.dart
@@ -1,38 +1,24 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:macros/macros.dart';
+import 'package:macros/macros.dart';
 
-// macro class Hello implements ClassDeclarationsMacro {
-//   const  Hello();
+macro class Hello implements ClassDeclarationsMacro {
+  const Hello();
 
-//   @override
-//   FutureOr<void> buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder) async {
-//     builder.declareInLibrary(
-//       DeclarationCode.fromParts([
-//         'augment ',
-//         'class ${clazz.identifier.name}',
-//         ' {',
-//       ]),
-//     );
+  @override
+  FutureOr<void> buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder) async {
+    final methods = await builder.methodsOf(clazz);
+    final hello =
+        methods.where((e) => e.identifier.name == 'hello').firstOrNull;
 
-//     final methods = await builder.methodsOf(clazz);
-//     final hello =
-//         methods.where((e) => e.identifier.name == 'hello').firstOrNull;
+    if (hello != null) return;
 
-//     if (hello != null) return;
-
-//     builder.declareInLibrary(
-//       DeclarationCode.fromParts(
-//         [
-//           '  void hello() => print("Hello, World!");',
-//         ]
-//       ),
-//     );
-
-//     builder.declareInLibrary(
-//       DeclarationCode.fromParts([
-//         '}',
-//       ]),
-//     );
-//   }
-// }
+    builder.declareInType(
+      DeclarationCode.fromParts(
+        [
+          '  void hello() => print("Hello, World!");',
+        ]
+      ),
+    );
+  }
+}
